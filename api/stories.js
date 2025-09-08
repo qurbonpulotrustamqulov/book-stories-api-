554755097e5d761e6faf30e3d40585e09309f893
Abdulloh <qurbonpulotrustamqulov@gmail.com>
2025-09-08 20:21:49 +0500

    Setup

--- api/stories.js
+++ api/stories.js
@@ -1,22 +1,27 @@
 import data from '../db.json';
 
 export default function handler(req, res) {
-  const { page = 1, limit = 6, id } = req.query;
+  const { query } = req;
+  const page = parseInt(query.page || "1");
+  const limit = parseInt(query.limit || "6");
+  const id = query.id ? parseInt(query.id) : null;
 
+  // detail (id bo'yicha)
   if (id) {
-    const story = data.stories.find(s => s.id === Number(id));
+    const story = data.stories.find(s => s.id === id);
     if (!story) return res.status(404).json({ message: "Not found" });
     return res.status(200).json(story);
   }
 
+  // list + pagination
   const start = (page - 1) * limit;
-  const end = start + Number(limit);
+  const end = start + limit;
   const paginated = data.stories.slice(start, end);
 
   res.status(200).json({
     total: data.stories.length,
-    page: Number(page),
-    limit: Number(limit),
+    page,
+    limit,
     data: paginated
   });
 }
